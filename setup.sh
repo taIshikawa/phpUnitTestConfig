750cdf176b6d76357e643dbbf413dd64800ca84d
taIshikawa <yatono0314@gmail.com>
2020-02-06 19:22:11 +0900

    test

--- setup.sh
+++ setup.sh
@@ -3,4 +3,11 @@
 cd ../
 mkdir data
 git clone https://github.com/taIshikawa/phpUnitTestWeb.git web
+# composeインスコ
 docker-compose exec workspace composer install
+# テストDB作成
+#docker-compose exec mysql mysql -u root -proot -e"grant create on *.* to 'default'@'%'"
+docker-compose exec mysql mysql -u root -proot -e"grant all on *.* to 'default'@'%'"
+docker-compose exec mysql mysql -u default -psecret -e'create database testing;'
+#権限確認
+docker-compose exec mysql mysql -u root -proot -e"SHOW GRANTS for 'default'@'%';"
